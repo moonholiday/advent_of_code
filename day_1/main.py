c3bb0492a61ecbd983cc1b6aa274bde344bba22e
moonholiday <727moonholiday@gmail.com>
2023-08-24 19:11:46 +0545

    day 2

---
+++ day_1/main.py
@@ -0,0 +1,33 @@
+file = open('input.txt', 'r')
+data = file.read()
+
+
+def day1_1():
+    floor = 0
+    for char in data:
+        if (char == "("):
+            floor += 1
+        elif (char == ")"):
+            floor -= 1
+    print(floor)
+
+
+def day1_2():
+    floor = 0
+    position = 0
+    for char in data:
+        position += 1
+        if (char == "("):
+            floor += 1
+            if (floor == -1):
+                break
+        elif (char == ")"):
+            floor -= 1
+            if (floor == -1):
+                break
+    print(position)
+
+
+if __name__ == '__main__':
+    day1_1()
+    day1_2()
